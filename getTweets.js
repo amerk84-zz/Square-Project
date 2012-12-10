99a4b57245f3ef4d316957c643b3f14fa6931db7
Amerk84 <andrew.moehrke@gmail.com>
2012-12-10 00:55:22 -0800

    updated to use recursion
    use recursion to get the 1000 tweets

--- getTweets.js
+++ getTweets.js
@@ -2,43 +2,86 @@
 $ = require('jquery');
 
 var n = 0;
-var i = 0;
+var tweets;
 var idNum = "0";
-var twit;
-var _url = 'https://api.twitter.com/1/statuses/user_timeline/willyferrell.json?callback=?&count=200';
+var lastCall = "no";
+var username = "justinbieber";
+var _url = 'https://api.twitter.com/1/statuses/user_timeline/'+username+'.json?callback=?&count=200';
+var allData=[]; //find out what variable type this should be
 
-while(n < 1000){
-    if(n>0) _url = 'https://api.twitter.com/1/statuses/user_timeline/willyferrell.json?callback=?&count=200&max_id='+idNum;
-    console.log(_url);
-    
-    $.ajax({
-        async:false,
-        url: _url,
-        dataType: 'json',
-        success: function(data){
-            twit = data;
-            console.log(_url);
-        }
-    });
-    
-    n += twit.length;
-    console.log(n);
-    var j = twit.length;
-    idNum = twit[j-1].id.toString();
-    console.log(idNum);
-    i++;
+//make initial tweet request
+$.ajax({
+    async:false,
+    url: _url,
+    dataType: 'json',
+    success: cb
+});
+
+function cb(data){
+	//console.log("checkpoint - 1");
+	//we know we are at a user's last tweet when the returned data array's length is one
+	if(data.length == 1) {
+		//console.log("checkpoint - 1.5");
+		pullTweets();
+		return;
+	}
+	//console.log("checkpoint - 2");
+	
+	//pull out tweet ID from the last tweet returned in the call
+    idNum = data[data.length-1].id.toString();
+
+	//remove first tweet, which is a duplicate of the last tweet from the previous request, only after first request
+	if(_url.indexOf("max_id") != -1) data.shift();
+	
+	//add contents of data to the allData array
+	allData = allData.concat(data);
+	if(allData.length < 1000){
+		_url = 'https://api.twitter.com/1/statuses/user_timeline/'+username+'.json?callback=?&count=200&max_id='+idNum;
+		$.ajax({
+	        async:false,
+	        url: _url,
+	        dataType: 'json',
+	        success: cb
+	    });
+		//console.log("checkpoint - 3");
+	}
+	else{
+		//console.log("checkpoint - 4");
+		pullTweets();
+	}
 }
 
+function pullTweets(){
+	//console.log("checkpoint - 5");
+	var len = (allData.length > 1000) ? 1000 : allData.length;
+    for(var i=0;i<len;i++){
+        tweets += " "+ allData[i].text;
+	}
+    var words = parseTweets();
+    //print final word count to the console
+    console.log("\nThe words that "+username+" has tweeted the most are:\n");
+    var iWordsCount = words.length;
+    for (var i=0; i<iWordsCount; i++) {
+        var word = words[i];
+        if(word.freq > 0) console.log(word.freq, word.text);
+    }
+    //console.log('\n' + username+" has tweeted " +len+ " times");
+}
 
-/*
-var words = (function(){
+function parseTweets(){
+	//console.log("checkpoint - 6");
     //put words into an array, removing links, hashtags, and mentions as well as any random characters
     var sWords = tweets.toLowerCase().trim().replace(/[#]+[A-Za-z0-9-_]+/g,'').replace(/[@]+[A-Za-z0-9-_]+/g,'').replace(/[+$~\`%-_,;:|."?!*{}”“]/g,'').split(/[\s\/]+/g).sort();
+	//remove words that contain "http"
+	for (var i=sWords.length-1; i>=0; i--) {
+	    if (sWords[i].indexOf("http") != -1) {
+	        sWords.splice(i, 1);
+	    }
+	}
     //save the word count before removing duplicates
     var iWordsCount = sWords.length;
-        
     // array of words to ignore
-    var ignores = ['and','the','to','a','of','for','as','i','with','it','is','on','that','this','can','in','be','has','if',"rt","gt","lt","amp","sp"];
+    var ignores = ["gt","lt","amp","sp"];
     ignores = (function(){
         var o = {};
         var iCount = ignores.length;
@@ -47,7 +90,6 @@ var words = (function(){
         }
         return o;
     }());
-        
 	//object to store the words in
     var counts = {};
     for (var i=0; i<iWordsCount; i++) {
@@ -57,7 +99,6 @@ var words = (function(){
             counts[sWord]++;
         }
     }
-        
     //create an array that will contain the objects with words and their frequency
 	//and the populate that array with each word and its corresponding frequency
     var wordArray = [];
@@ -67,19 +108,8 @@ var words = (function(){
             freq: counts[sWord]
         });
     }
-        
     //sort array by descending frequency
     return wordArray.sort(function(a,b){
         return (a.freq > b.freq) ? -1 : ((a.freq < b.freq) ? 1 : 0);
     });
-}());
-    
-//print final word count to the console
-console.log("The words that "+username+" has tweeted the most are:");
-var iWordsCount = words.length;
-for (var i=0; i<iWordsCount; i++) {
-    var word = words[i];
-    if(word.freq > 1) console.log(word.freq, word.text);
 }
-console.log(username+" has tweeted " +data.length+ " times");
-*/
